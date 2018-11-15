f6528f906c615d38da556ef81e5ef980f07ba5e2
Anil Bunkar <anilkumar4392@gmail.com>
2018-11-15 12:09:40 +0530

    first commit

---
+++ ABFancyLabel.framework/Headers/DummyClass.h
@@ -0,0 +1,13 @@
+//
+//  DummyClass.h
+//  FirstFrameworkProject
+//
+//  Created by Anil Bunkar on 04/10/18.
+//  Copyright © 2018 Adobe Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyClass : NSObject
++ (void) welcomeMessage;
+@end
---
+++ ABFancyLabel.framework/Headers/FirstFrameworkProject.h
@@ -0,0 +1,20 @@
+//
+//  FirstFrameworkProject.h
+//  FirstFrameworkProject
+//
+//  Created by Anil Bunkar on 04/10/18.
+//  Copyright © 2018 Adobe Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DummyClass.h"
+
+//! Project version number for FirstFrameworkProject.
+FOUNDATION_EXPORT double FirstFrameworkProjectVersionNumber;
+
+//! Project version string for FirstFrameworkProject.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFrameworkProject/PublicHeader.h>
+
+
